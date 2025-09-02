d77154066e1b25e34511cb426204e20a3a9b38de
Harsh kumar <harshk0825@gmail.com>
2025-09-02 22:40:21 +0530

    police dahboard made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,25 @@
 import { useState } from 'react'
+import { Routes, Route } from "react-router-dom";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Sidebar from "./components/sidebar";
-import HomePagr from './components/HomePagr';
+import HomePagr from './Pages/User_dashboard';
+import Police_dashboard from './Pages/Police_dashboard';
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <> <div className='flex'>
+    <div>
 
-        <Sidebar/>
-      <HomePagr/>
+    <Sidebar/>
+   <Routes>
+  <Route path="/" element={<Police_dashboard />} />
+</Routes>
+
     </div>
-    </>
+  
   )
 }
 
