6cf0947b36cdb4889c8f5d9eee30c01fa6a563cb
RudrakshChugh <rudrakshchugh005@gmail.com>
2025-09-13 01:27:08 +0530

    added FIR page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import FloatingSidebar from './components/Sidebar';
 import FloatingNavbar from './components/Navbar';
 import Police_dashboard from "./Pages/Dashboard";
 import Map_dashboard from "./Pages/Map";
+import FIR from "./Pages/FIR";
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -14,6 +15,7 @@ function App() {
         {/* <Route path="/" element={</>} /> */}
         <Route path="/" element={<Police_dashboard />} />
         <Route path="/map" element={<Map_dashboard />} />
+        <Route path="/cases" element={<FIR />} />
       </Routes>
     </div>
   );
